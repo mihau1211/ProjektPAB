1cc2f4022d9f56ea400c649ef5679c20f0a36fc7
mihau1211 <mihau1211@users.noreply.github.com>
2020-10-21 16:40:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import Connection.*;
 
 import java.lang.reflect.AccessibleObject;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        Scanner scan = new Scanner(System.in);
+
         ConnectToSql connectToSql;
         connectToSql=new ConnectToSql();
         connectToSql.startConnection();
@@ -17,12 +20,11 @@ public class Main {
         Movie movie = new Movie();
         Type type = new Type();
 
-//        insert.addCountry(connectToSql.conn, "POLAND");
-//        insert.addType(connectToSql.conn, "Action");
-//        insert.addDirector(connectToSql.conn, "TMP Director", "1982-09-12", 1);
-//        insert.addMovie(connectToSql.conn, "TMP movie", "2020-01-01", 8, 1, 1, 1);
-//        insert.addActor(connectToSql.conn, "TMP Actor", "1948-04-12", 1);
-//        insert.addActorHasMovie(connectToSql.conn, 2,2);
-
+//        country.insertCountry(connectToSql.conn, "POLAND");
+//        type.insertType(connectToSql.conn, "Action");
+//        director.insertDirector(connectToSql.conn, "TMP Director", "1982-09-12", 1);
+//        movie.insertMovie(connectToSql.conn, "TMP movie", "2020-01-01", 8, 1, 1, 1);
+//        actor.insertActor(connectToSql.conn, "TMP Actor", "1948-04-12", 1);
+//        actorHasMovie.insertActorHasMovie(connectToSql.conn, 1, 1);
     }
 }
