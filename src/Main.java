0005a46285af41adcfd4ab4eb588e8e26164de01
mihau1211 <mihau1211@users.noreply.github.com>
2020-10-28 11:15:03 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -22,66 +22,63 @@ public class Main {
 
         boolean end=true;
         int switcher;
-        if (connectToSql.conn!=null){
-            while(end){
-                System.out.println("================================================================================");
-                System.out.println("| 1. Dodaj film");
-                System.out.println("| 2. Dodaj aktora");
-                System.out.println("| 3. Pokaz filmy");
-                System.out.println("| 4. Pokaz aktorow");
-                System.out.println("| 5. Pokaz rezyserow");
-                System.out.println("| 6. Usun film");
-                System.out.println("| 7. Usun aktora");
-                System.out.println("| 8. Zmodyfikuj film");
-                System.out.println("| 9. Zmodyfikuj aktora");
-                System.out.println("| 0. Wyjdz");
-                System.out.println("================================================================================");
+        while(end){
+            System.out.println("================================================================================");
+            System.out.println("| 1. Dodaj film");
+            System.out.println("| 2. Dodaj aktora");
+            System.out.println("| 3. Pokaz filmy");
+            System.out.println("| 4. Pokaz aktorow");
+            System.out.println("| 5. Pokaz rezyserow");
+            System.out.println("| 6. Usun film");
+            System.out.println("| 7. Usun aktora");
+            System.out.println("| 8. Zmodyfikuj film");
+            System.out.println("| 9. Zmodyfikuj aktora");
+            System.out.println("| 0. Wyjdz");
+            System.out.println("================================================================================");
 
-                long numberID;
-                switcher = scan.nextInt();
-                switch(switcher){
-                    case 1:
-                        movie.insertMovie(connectToSql.conn);
-                        break;
-                    case 2:
-                        actor.insertActor(connectToSql.conn);
-                        break;
-                    case 3:
-                        System.out.printf("| ID| ---------- | RATING | ---------- | MOVIE TITLE | ---------- | RELEASE DATE |\n");
-                        movie.selectMovie(connectToSql.conn);
-                        break;
-                    case 4:
-                        System.out.printf("| ID| ---------- | ACTOR NAME | ---------- | ACTOR BIRTH DATE |\n");
-                        actor.selectActor(connectToSql.conn);
-                        break;
-                    case 5:
-                        System.out.printf("| ID| ---------- | DIRECTOR NAME | ---------- | DIRECTOR BIRTH DATE |\n");
-                        director.selectDirector(connectToSql.conn);
-                        break;
-                    case 6:
-                        System.out.println("Podaj ID filmu do usuniecia:");
-                        movie.deleteMovieByID(connectToSql.conn);
-                        break;
-                    case 7:
-                        System.out.println("Podaj ID aktora do usuniecia:");
-                        actor.deleteActorByID(connectToSql.conn);
-                        break;
-                    case 8:
-                        System.out.println("Podaj id filmu ktory chcesz zmodyfikowac:");
-                        numberID = scan.nextLong();
-                        movie.updateMovie(connectToSql.conn, numberID);
-                        break;
-                    case 9:
-                        System.out.println("Podaj id aktora ktorego chcesz zmodyfikowac:");
-                        numberID = scan.nextLong();
-                        actor.updateActor(connectToSql.conn, numberID);
-                        break;
-                    case 0:
-                        end=false;
-                        System.out.println("Bye!!!!!!!!!");
-                        break;
+            long numberID;
+            switcher = scan.nextInt();
+            switch(switcher){
+                case 1:
+                    movie.insertMovie(connectToSql.conn);
+                    break;
+                case 2:
+                    actor.insertActor(connectToSql.conn);
+                    break;
+                case 3:
+                    System.out.printf("| ID| ---------- | RATING | ---------- | MOVIE TITLE | ---------- | RELEASE DATE |\n");
+                    movie.selectMovie(connectToSql.conn);
+                    break;
+                case 4:
+                    actor.selectActor(connectToSql.conn);
+                    break;
+                case 5:
+                    System.out.printf("| ID| ---------- | DIRECTOR NAME | ---------- | DIRECTOR BIRTH DATE |\n");
+                    director.selectDirector(connectToSql.conn);
+                    break;
+                case 6:
+                    System.out.println("Podaj ID filmu do usuniecia:");
+                    movie.deleteMovieByID(connectToSql.conn);
+                    break;
+                case 7:
+                    System.out.println("Podaj ID aktora do usuniecia:");
+                    actor.deleteActorByID(connectToSql.conn);
+                    break;
+                case 8:
+                    System.out.println("Podaj id filmu ktory chcesz zmodyfikowac:");
+                    numberID = scan.nextLong();
+                    movie.updateMovie(connectToSql.conn, numberID);
+                    break;
+                case 9:
+                    System.out.println("Podaj id aktora ktorego chcesz zmodyfikowac:");
+                    numberID = scan.nextLong();
+                    actor.updateActor(connectToSql.conn, numberID);
+                    break;
+                case 0:
+                    end=false;
+                    System.out.println("Bye!!!!!!!!!");
+                    break;
 
-                }
             }
         }
     }
