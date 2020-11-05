485f1a91d10cf72ca386f0343d5895aeeec07014
mihau1211 <mihau1211@users.noreply.github.com>
2020-11-05 20:14:01 +0100

    Update MyFrame.java

--- src/Window/MyFrame.java
+++ src/Window/MyFrame.java
@@ -3,37 +3,16 @@ package Window;
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class MyFrame extends JFrame{
     public MyFrame() {
         super("Hello World");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700,500);
+        MenuBar mb = new MenuBar();
 
-        JMenu menuActor, menuDirector;
-        JMenuItem itemAdd, itemDelete, itemSelect, itemUpdate;
-
-        JMenuBar mbActor=new JMenuBar();
-        menuActor=new JMenu("ACTOR");
-        itemAdd=new JMenuItem("ADD");
-        itemDelete=new JMenuItem("DELETE");
-        itemSelect=new JMenuItem("SELECT");
-        itemUpdate=new JMenuItem("UPDATE");
-        menuActor.add(itemAdd); menuActor.add(itemDelete); menuActor.add(itemSelect); menuActor.add(itemUpdate);
-        mbActor.add(menuActor);
-        setJMenuBar(mbActor);
-
-        JMenuBar mbDirector=new JMenuBar();
-        menuActor=new JMenu("DIRECTOR");
-        itemAdd=new JMenuItem("ADD");
-        itemDelete=new JMenuItem("DELETE");
-        itemSelect=new JMenuItem("SELECT");
-        itemUpdate=new JMenuItem("UPDATE");
-        menuActor.add(itemAdd); menuActor.add(itemDelete); menuActor.add(itemSelect); menuActor.add(itemUpdate);
-        mbDirector.add(menuActor);
-        setJMenuBar(mbDirector);
-        mbActor.setBounds(0,0,20,10);
-
+        setJMenuBar(mb.mb);
         setLayout(null);
         setVisible(true);
     }
