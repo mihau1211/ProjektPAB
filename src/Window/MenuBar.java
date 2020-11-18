954c288a11868cac2bcb9ebecf76e3da2c70609f
mihau1211 <mihau1211@users.noreply.github.com>
2020-11-18 10:47:17 +0100

    Update MenuBar.java

--- src/Window/MenuBar.java
+++ src/Window/MenuBar.java
@@ -11,15 +11,17 @@ public class MenuBar{
     String[] tables = {"ACTOR", "DIRECTOR", "MOVIE", "COUNTRY", "TYPE", "AHM"};
     String[] items = {"ADD","DELETE","SELECT","UPDATE"};
     JMenuBar mb = new JMenuBar();
-    JMenu menus[] = new JMenu[tables.length];
+    JMenu menus[] = new JMenu[items.length];
 
     MenuBar() {
 
         //dodanie obiektow menu w aplikacji
-        for (int i = 0; i < tables.length; i++) {
-            menus[i] = new JMenu(tables[i]);
-            for (int j = 0; j < items.length; j++) {
-                menus[i].add(new JMenuItem(items[j]));
+        for (int i = 0; i < items.length; i++) {
+            menus[i] = new JMenu(items[i]);
+            for (int j = 0; j < tables.length; j++) {
+                if (i!=3 || j!=5) {
+                    menus[i].add(new JMenuItem(tables[j]));
+                }
             }
             mb.add(menus[i]);
         }
