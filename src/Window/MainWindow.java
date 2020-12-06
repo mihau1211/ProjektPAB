5e902f0e5aaf1fe847c7b43d165d89238aa373fa
mihau1211 <mihau1211@users.noreply.github.com>
2020-12-06 17:04:58 +0100

    Update MainWindow.java

--- src/Window/MainWindow.java
+++ src/Window/MainWindow.java
@@ -1,9 +1,10 @@
 package Window;
 
 import java.awt.*;
+import java.io.IOException;
 
 public class MainWindow {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
