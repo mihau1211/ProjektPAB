716deb61f502324f1c128e7015e14aa4947ee8c0
mihau1211 <mihau1211@users.noreply.github.com>
2020-12-06 17:04:44 +0100

    Update ConnectToSql.java

--- src/Connection/ConnectToSql.java
+++ src/Connection/ConnectToSql.java
@@ -1,8 +1,14 @@
 package Connection;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Hashtable;
+import java.util.Properties;
 
 public class ConnectToSql {
     private final String USER_NAME="";
@@ -12,8 +18,52 @@ public class ConnectToSql {
     private final String CONNECTION_PROPS="";
     private final String SERVER_NAME="";
     public Connection conn = null;
+
+
+
     public void startConnection() {
         System.out.println("Start");
+
+        Hashtable<String, String> hashtable = new Hashtable<String, String>();
+
+        hashtable.put("host", "127.0.0.1");
+        hashtable.put("user", "root");
+
+        System.out.println(hashtable.get(0));
+
+        Properties applicationProps = new Properties();
+        applicationProps.setProperty("host", "127.0.0.1");
+        applicationProps.setProperty("port", "55555");
+        applicationProps.setProperty("user", "root");
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream("appProperties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            applicationProps.storeToXML(out, "---No Comment---");
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream("appProperties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            applicationProps.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println(applicationProps.getProperty("host", "127.0.0.1"));
 
         try {
             conn =
